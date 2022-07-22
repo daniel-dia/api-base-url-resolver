1d4faecf3c2494b2ff47669db0bfd6f3cd1a5c4e
daniel santos <daniel.dia@outlook.com>
2022-07-22 10:02:00 -0300

    add to string to stryker

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export default function subDomainResolver(origin: string, subDomain: string, opt
         if (options.envVar && process.env[options.envVar]) return process.env[options.envVar];
         if (options.conditions?.[origin]) return options.conditions[origin];
         if (options.replace) url.hostname = url.hostname.slice(url.hostname.indexOf(".") + 1);
-        if (options.protocol) url.protocol = options.protocol;
+        if (options.protocol) url.protocol = options.protocol.toString();
     }
 
     url.hostname = subDomain + "." + url.hostname;
